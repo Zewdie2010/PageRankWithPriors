10e41b7f169828a25a3e34fc1189e45dba584c72
WH <wanghaovip2020@gmail.com>
2016-03-14 11:59:54 +0800

    graph class

--- src/main/java/org/wanghao/Graph.java
+++ src/main/java/org/wanghao/Graph.java
@@ -1,5 +1,8 @@
 package org.wanghao;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map.Entry;
@@ -28,8 +31,168 @@ public class Graph {
 	Random random;
 	public double Error = 0.000000001;
 
-	public Graph() {
+	public Graph(String graphfile, String nodeNamefile) {
 
+		this.amountEdge = 0;
+		this.amountVex = 0;
+		this.totalWeight = 0;
+		this.vertexList = new LinkedList<Vertex>();
+		this.Id_InWeight = new HashMap<Integer, Double>();
+		this.Id_OutWeight = new HashMap<Integer, Double>();
+		this.PageRankValue = new LinkedList<Double>();
+		this.random = new Random();
+		this.graphFilename = graphfile;
+
+		readGraph();
+		GetUserName(nodeNamefile);
+
+	}
+
+	public Graph() {
+		this.amountVex = 0;
+		this.amountEdge = 0;
+		this.totalWeight = 0;
+		this.vertexList = new LinkedList<Vertex>();
+		this.Id_OutWeight = new HashMap<Integer, Double>();
+		this.Id_InWeight = new HashMap<Integer, Double>();
+		random = new Random();
+		this.PageRankValue = new LinkedList<Double>();
+	}
+
+	// get the author name from the "list.txt"
+	public void GetUserName(String filename) {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File(filename)));
+			String line = "";
+			while ((line = reader.readLine()) != null) {
+				String[] tokens = line.split("\t");
+				if (tokens.length == 2) {
+					int id = Integer.valueOf(tokens[1]);
+					this.vertexList.get(id).name = tokens[0];
+				}
+			}
+			reader.close();
+		} catch (Exception e) {
+			System.err.println("Read the list.txt wrong");
+		}
+	}
+
+	// remove the edge from A to B
+	public void RemoveEdge(int idA, int idB) {
+
+	}
+
+	// clone the graph
+	@Override
+	public Graph clone() {
+
+		return null;
+	}
+
+	// create the edge from A to B
+	private void CreateEdge(int idA, int idB, double weight) {
+
+		this.vertexList.get(idA).outdegree++;
+		this.vertexList.get(idA).outweight += weight;
+		this.vertexList.get(idB).indegree++;
+		this.vertexList.get(idB).inweight += weight;
+
+		double tranP = 0.1;
+		// out edge
+		if (this.vertexList.get(idA).edges.containsKey(idB)) {
+			this.vertexList.get(idA).edges.get(idB).weight += weight;
+		} else {
+			this.vertexList.get(idA).edges.put(idB, new Edge(idB, weight, tranP));
+		}
+		// in edge
+		if (this.vertexList.get(idB).redges.containsKey(idA)) {
+			this.vertexList.get(idB).redges.get(idA).weight += weight;
+		} else {
+			this.vertexList.get(idB).redges.put(idA, new Edge(idA, weight, tranP));
+		}
+
+	}
+
+	// read the graph from the graphfile
+	private void readGraph() {
+
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(new File(this.graphFilename)));
+			String line = "";
+			LinkedList<LinkedList<String>> lineList = new LinkedList<LinkedList<String>>();
+			int MaxId = 0;
+
+			// ******************* 1. find the max Id vertex
+			while ((line = reader.readLine()) != null) {
+
+				String[] tokens = line.split("\t");
+
+				if (tokens.length < 2 || tokens.length > 3)
+					continue;
+
+				int IdA = Integer.valueOf(tokens[0]);
+				int IdB = Integer.valueOf(tokens[1]);
+
+				if (MaxId < IdA)
+					MaxId = IdA;
+				if (MaxId < IdB)
+					MaxId = IdB;
+			}
+			reader.close();
+
+			// ********************** 2. create the graph vertex
+			for (int i = 0; i < MaxId; i++) {
+				this.vertexList.add(new Vertex());
+			}
+
+			// ********************** 3. create the graph edge
+			// also create the graph
+			// read the file again
+			reader = new BufferedReader(new FileReader(new File(this.graphFilename)));
+			while ((line = reader.readLine()) != null) {
+
+				String[] tokens = line.split("\t");
+				if (tokens.length < 2 || tokens.length > 3)
+					continue;
+
+				int idA = Integer.valueOf(tokens[0]);
+				int idB = Integer.valueOf(tokens[1]);
+				double weight;
+				if (tokens.length == 3)
+					weight = Double.valueOf(tokens[2]);
+				else
+					weight = 1;
+
+				CreateEdge(idA, idB, weight);
+				CreateEdge(idB, idA, weight);
+
+				this.amountEdge++;
+
+			}
+			reader.close();
+			this.amountVex = this.vertexList.size();
+
+			// *****4. get the transmission probability between the vertex
+			// P(idA->idB) = EdgeWeight(idA->idB)/idB.inWeight
+			for (int i = 0; i < this.amountVex; i++) {
+				for (Entry<Integer, Edge> entry : this.vertexList.get(i).edges.entrySet()) {
+					entry.getValue().tranP = entry.getValue().weight / this.vertexList.get(entry.getKey()).inweight;
+					this.vertexList.get(entry.getKey()).redges.get(i).tranP = entry.getValue().tranP;
+				}
+			}
+
+			// ********5 . count the vertices and edges
+			for (int i = 0; i < this.amountVex; i++) {
+				this.Id_InWeight.put(i, this.vertexList.get(i).inweight);
+				this.Id_OutWeight.put(i, this.vertexList.get(i).outweight);
+				if (this.vertexList.get(i).edges.size() != 0)
+					this.totalWeight += this.vertexList.get(i).outweight;
+			}
+
+		} catch (Exception e) {
+			System.out.println("Network IO Exception");
+		}
 	}
 
 	/************************************************************************
--- src/main/java/org/wanghao/test.java
+++ src/main/java/org/wanghao/test.java
@@ -1,8 +1,8 @@
 package org.wanghao;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class test {
 
@@ -12,14 +12,32 @@ public class test {
 		try {
 			File file = new File("src/main/resources/Data/AimedPaper.txt");
 			System.out.println(file.exists());
-			BufferedReader bufferreader = new BufferedReader(new FileReader(file));
 			String line = "";
-			while ((line = bufferreader.readLine()) != null) {
-				System.out.println(line);
-			}
-		} catch (Exception e) {
+			// while ((line = bufferreader.readLine()) != null) {
+			// System.out.println(line);
+			// }
+		} catch (
+
+		Exception e)
+
+		{
 			System.out.println("the buffer reader wrong");
 		}
+
+		Random random = new Random();
+		for (int i = 0; i < 10; i++) {
+			System.out.println("The i = " + i + random.nextDouble());
+		}
+
+		System.out.println("***************");
+		LinkedList<String> link = new LinkedList<String>();
+		link.add("0");
+		link.add("1");
+		link.add("2");
+		link.add("3");
+		link.add("4");
+		for (int i = 0; i < link.size(); i++)
+			System.out.println(link.get(i));
 	}
 
 }
